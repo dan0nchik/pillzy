a3e70b737499218789787748ab074a120d4a854d
Daniel Khromov <danielkhromov@gmail.com>
2020-04-23 14:35:45 +0300

    create app

---
+++ Pillzy/Pillzy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pillzy
+//
+//  Created by Daniel Khromov on 4/23/20.
+//  Copyright © 2020 Daniel Khromov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+        Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
