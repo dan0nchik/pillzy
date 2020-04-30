9f9529aea9822b85bd4bfb5d8ee0a27391990965
Daniel Khromov <danielkhromov@gmail.com>
2020-04-30 11:17:47 +0300

    2 page ready and  notifications work with pill names

--- Pillzy/Pillzy/AddPill.swift
+++ Pillzy/Pillzy/AddPill.swift
@@ -12,86 +12,129 @@ import RealmSwift
 
 class Pill: Object {
     @objc dynamic var name = ""
+    @objc dynamic var meal = ""
 }
 
 struct AddPill: View {
     @State var pillName = ""
+    @State var meal = ""
+    @State private var remind = Date()
+    @State var onTapBeforeMeal = false
+    @State var onTapWithMeal = false
+    @State var onTapAftereMeal = false
+    let pill = Pill()
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        ZStack{
-        Color("Background")
-            .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .leading) {
-              Text("Add").font(.system(.largeTitle, design: .rounded)).bold().padding()
-                    Text("Pills name").bold()
-                    .padding()
-                TextField("Name", text: $pillName)
-                    .padding()
-                    .background(Color("LighterBackground").cornerRadius(20))
-                    .padding()
-                    Text("Notification").bold()
+            ZStack{
+                Color("Background")
+                    .edgesIgnoringSafeArea(.all)
+                ScrollView{
+                VStack(alignment: .leading) {
+                  Text("Add").font(.system(.largeTitle, design: .rounded)).bold().padding()
+                        Text("Pills name").bold()
                         .padding()
                     TextField("Name", text: $pillName)
                         .padding()
                         .background(Color("LighterBackground").cornerRadius(20))
                         .padding()
-                
-                Button(action: {
-                    let content = UNMutableNotificationContent()
-                    content.title = "Feed the cat"
-                    content.subtitle = "It looks hungry"
-                    content.sound = UNNotificationSound.default
-
-                    // show this notification five seconds from now
-                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                    // choose a random identifier
-                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                    // add our notification request
-                    UNUserNotificationCenter.current().add(request)
-                }, label: {Text("n").padding()})
-                
-                Button(action: {
-                    if(self.pillName != ""){
-                    do{
-                        let pill = Pill()
-                        
-                        pill.name = self.pillName
-                        try realm.write({
-                            realm.add(pill)
-                            for i in res{
-                                print(i.name)
-                            }
-                        })
-                        
-                    }
-                    catch{
-                        print(error.localizedDescription)
-                    }
-                    }
-                    self.presentationMode.wrappedValue.dismiss()
-                }, label: {
-                    Text("Save").font(.title).bold()
-                       .frame(minWidth: 0, maxWidth: .infinity)
-                    .padding()
-                        .foregroundColor(.pink)
-                        .background(Color("Background"))
-                        .cornerRadius(20)
-                        .padding()
-                        .lightShadow()
-                        .darkShadow()
                         
                     
-                })
-                Spacer()
-        }
-        }.onAppear {
-            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                if success {
-                    print("All set!")
-                } else if let error = error {
-                    print(error.localizedDescription)
+    //                Button(action: {
+    //                    let content = UNMutableNotificationContent()
+    //                    content.title = "Feed the cat"
+    //                    content.subtitle = "It looks hungry"
+    //                    content.sound = UNNotificationSound.default
+    //
+    //                    // show this notification five seconds from now
+    //                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+    //
+    //                    // choose a random identifier
+    //                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+    //
+    //                    // add our notification request
+    //                    UNUserNotificationCenter.current().add(request)
+    //                }, label: {Text("n").padding()})
+                    
+                    Text("Food & pills").bold()
+                    .padding()
+                    HStack{
+                        food(align: "before").onTapGesture {
+                            self.onTapBeforeMeal = true
+                            self.onTapAftereMeal = false
+                            self.onTapWithMeal = false
+                            self.pill.meal = "Before"
+                        }.foregroundColor(onTapBeforeMeal ? .pink : Color("LighterBackground"))
+                        food(align: "with").onTapGesture {
+                            self.onTapBeforeMeal = false
+                            self.onTapAftereMeal = false
+                            self.onTapWithMeal = true
+                            self.pill.meal = "With"
+                        }.foregroundColor(onTapWithMeal ? .pink : Color("LighterBackground"))
+                        food(align: "after").onTapGesture {
+                            self.onTapBeforeMeal = false
+                            self.onTapAftereMeal = true
+                            self.onTapWithMeal = false
+                            self.pill.meal = "After"
+                        }.foregroundColor(onTapAftereMeal ? .pink : Color("LighterBackground"))
+                    }
+                    Text("Notification").bold()
+                        .padding()
+                    DatePicker("", selection: $remind, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    Button(action: {
+                        if(self.pillName != ""){
+                        do{
+                            self.pill.name = self.pillName
+                            try realm.write({
+                                realm.add(self.pill)
+                                for i in res{
+                                    print(i.name)
+                                    print(i.meal)
+                                }
+                            })
+                            
+                        }
+                        catch{
+                            print(error.localizedDescription)
+                        }
+                        }
+                        let content = UNMutableNotificationContent()
+                        content.title = "Drink \(self.pill.name)"
+                        content.subtitle = "\(self.pill.meal) meal"
+                        content.sound = UNNotificationSound.default
+                        
+                        // show this notification five seconds from now
+                        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                        
+                        // choose a random identifier
+                        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                        
+                        // add our notification request
+                        UNUserNotificationCenter.current().add(request)
+                        self.presentationMode.wrappedValue.dismiss()
+                        
+                    }, label: {
+                        Text("Save").font(.title).bold().foregroundColor(.white)
+                           .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .foregroundColor(.pink)
+                            .background(Color(.systemPink))
+                            .cornerRadius(20)
+                            .padding()
+                            .lightShadow()
+                            .darkShadow()
+                            
+                        
+                    })
+                    Spacer()
+            }
+            }.onAppear {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        //print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
                 }
             }
         }
@@ -102,5 +145,41 @@ struct AddPill: View {
 struct AddPill_Previews: PreviewProvider {
     static var previews: some View {
         AddPill()
+    }
+}
+
+extension View
+{
+    func food(align: String) -> some View{
+        
+        return
+            ZStack{
+                RoundedRectangle(cornerRadius: 20).frame(width: 100, height: 90).padding()
+                if(align == "before"){
+                    HStack{
+                        Image(align)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 90, height: 90)
+                }
+                    
+                }
+                if(align == "with"){
+                HStack{
+                    Image(align)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 90)
+                }
+                }
+                if(align == "after"){
+                HStack{
+                    Image(align)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 90, height: 90)
+                }
+                }
+            }
     }
 }
